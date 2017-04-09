ec6fd341720d775ba5d766820d1795685b72a805
parkjongkook <parkjongkook@218.38.137.27>
2017-04-09 20:40:39 +0900

    JWT

---
+++ first/src/main/java/first/common/util/JwtService.java
@@ -0,0 +1,61 @@
+package first.common.util;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+@Component("jwtService")
+public class JwtService   {
+	protected Log log = LogFactory.getLog(EncryptionService.class);
+	
+	public String getToken(String userId) throws Exception{
+		String token="";
+		try{
+			Algorithm algorithm = Algorithm.HMAC256("secret");
+			Map<String, Object> headerClaims = new HashMap();
+			headerClaims.put("owner", "LeeParkOh");
+			     token = JWT.create()
+						    .withIssuer("LeeParkOh")
+						    .withHeader(headerClaims)
+						    .withClaim("userId", userId)
+						    .sign(algorithm);
+			 log.debug("token>>>>>"+ token); 
+		}catch(UnsupportedEncodingException exception){
+			log.debug("UnsupportedEncodingException>>>");
+			return "";
+		}catch (JWTVerificationException exception){
+			log.debug("JWTVerificationException>>>");
+			return "";
+		}
+		return token;
+	}
+	
+	public String verifyToken(String token) throws Exception{
+		try{
+			Algorithm algorithm = Algorithm.HMAC256("secret");
+			 JWTVerifier verifier = JWT.require(algorithm)
+								        .withIssuer("LeeParkOh")
+								        .build(); //Reusable verifier instance
+			 DecodedJWT jwt = verifier.verify(token);
+			 log.debug("jwt>>>"+jwt);
+		}catch(UnsupportedEncodingException exception){
+			log.debug("UnsupportedEncodingException>>>"+exception);
+			return "UnVerifiedToken";
+		}catch (JWTVerificationException exception){
+			log.debug("JWTVerificationException>>>"+exception);
+			return "UnVerifiedToken";
+		}
+		return "VerifiedToken";
+	}
+	 
+}
